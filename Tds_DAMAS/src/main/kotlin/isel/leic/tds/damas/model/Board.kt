a519aa451c09089152f53d30c0ee2ded66231554
Bernardo Jaco <148328839+jacaoo@users.noreply.github.com>
2024-10-20 00:20:11 +0100

    Add files via upload

---
+++ Tds_DAMAS/src/main/kotlin/isel/leic/tds/damas/model/Board.kt
@@ -0,0 +1,36 @@
+import isel.leic.tds.damas.model.*
+
+const val BOARD_DIM = 8
+const val BOARD_CELLS = BOARD_DIM*BOARD_DIM
+const val playerPieces = 12
+
+enum class Player{
+    w, b;
+    val other get() = if(this == w) b else w
+}
+typealias Move = Pair<Square, Piece?> // É para associar um quadrado com uma peça ( w, b )
+
+typealias Moves = Map<Square, Piece?> // É uma espécie de lista para armazenar todos os possíveis espaços do tabuleiro
+
+fun Moves.renew (old: Square, new: Piece?) = this.plus(Move(old, new)) // Serve para adicionar a peça no board
+
+class Board(val moves: Moves, val player: Player)
+
+fun initialBoard(): Board {
+    val init = emptyMap<Square, Piece?>()
+    Square.values.forEach { init.renew(it, null) }
+    var pieces = 0
+    var reverse = BOARD_CELLS - 1
+    for ( i in 0..<BOARD_CELLS-1) {
+        if (Square(i).black){
+            init.renew(Square.values[i], Piece(Player.b))
+            init.renew(Square.values[reverse], Piece(Player.w))
+            pieces++
+            reverse--
+            if (pieces == playerPieces) break
+        }
+    }
+    return Board(init, Player.w)
+}
+
+
