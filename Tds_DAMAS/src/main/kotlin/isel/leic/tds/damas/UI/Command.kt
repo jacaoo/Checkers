83294dcca9f537591becc9c7daf6f968111a9a25
Luka Castro Roca <154425333+LukaRoca@users.noreply.github.com>
2024-10-20 19:16:42 +0100

    Update Command.kt

--- Tds_DAMAS/src/main/kotlin/isel/leic/tds/damas/UI/Command.kt
+++ Tds_DAMAS/src/main/kotlin/isel/leic/tds/damas/UI/Command.kt
@@ -24,7 +24,7 @@ fun getCommands(storage: Storage<String, Board>) = mapOf(
     "REFRESH" to object : Command() {
         override fun execute(args: List<String>, g: Game?): Game {
             checkNotNull(g){ " Game not created. "} //se o jogo n for criado e null e da illegal state
-            val s = storage.read(id = g.id)
+            val s = storage.read(id = g.id) //basicamente esta função read da interface sotrage lê o id que passas como parametro e aqui, basicamente colocamos no s o id de g porque precisamos da informaçao de como se encontra o tabuleiro g
             require(s != null) { "Game not created." } //n percebi mt bem mas sem isto nunca se tinha a certeza se o board s era null ou nao
             return g.copy(board = s) //returna o mesmo game so que com o board ja alterado com a jogada q veio do outro terminal de comandos
         }
