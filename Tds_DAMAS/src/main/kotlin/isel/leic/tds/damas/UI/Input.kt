d79723fb68d36971b4393ac3114f78c55f778a12
jaco <bernardoesjaco@gmail.com>
2024-10-20 18:22:02 +0100

    feito o START e o EXIT e algumas alterações como o createGame, e o Input feito também

---
+++ Tds_DAMAS/src/main/kotlin/isel/leic/tds/damas/UI/Input.kt
@@ -0,0 +1,14 @@
+package isel.leic.tds.damas.UI
+
+data class LineCommand (val name: String, val params: List<String>)
+
+fun readCommand() : LineCommand {
+    while (true) {
+        print(">")
+        val words = readln().trim().uppercase().split(" ")
+        return if (words.isNotEmpty() && words.first().isNotBlank()) {
+            LineCommand(words.first().uppercase(),words.drop(1))
+        }
+        else readCommand()
+    }
+}
