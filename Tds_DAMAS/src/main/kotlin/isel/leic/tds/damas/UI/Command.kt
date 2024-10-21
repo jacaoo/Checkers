ff3a22bda55173a207f8f8e781a8c92556a2ceba
Luka Castro Roca <154425333+LukaRoca@users.noreply.github.com>
2024-10-21 15:13:02 +0100

    Update Command.kt

--- Tds_DAMAS/src/main/kotlin/isel/leic/tds/damas/UI/Command.kt
+++ Tds_DAMAS/src/main/kotlin/isel/leic/tds/damas/UI/Command.kt
@@ -8,7 +8,17 @@ abstract class Command(val syntax: String = ""){
     open fun execute(args: List<String>, g: Game?): Game? = g
     open val isTerminate: Boolean get() = false
 }
-
+object PlayCommand :Command("<position>"){
+    override fun execute(args: List<String>, g: Game?): Game {
+        checkNotNull(g) { "Game not created." }
+        require(args.isNotEmpty()) { "Missing position" }
+        val from = args.firstOrNull()?.toSquareOrNull()
+        requireNotNull(from) { "Invalid position ${args.firstOrNull()}" }
+        val to = args.lastOrNull()?.toSquareOrNull()
+        requireNotNull(to) { "Invalid position ${args.lastOrNull()}" }
+        return g.play(from, to)
+    }
+}
 fun getCommands(storage: Storage<String, Board>) = mapOf(
     "EXIT" to object : Command() {
         override val isTerminate: Boolean get() = true
@@ -27,9 +37,9 @@ fun getCommands(storage: Storage<String, Board>) = mapOf(
             val s = storage.read(id = g.id) //basicamente esta função read da interface sotrage lê o id que passas como parametro e aqui, basicamente colocamos no s o id de g porque precisamos da informaçao de como se encontra o tabuleiro g
             require(s != null) { "Game not created." } //n percebi mt bem mas sem isto nunca se tinha a certeza se o board s era null ou nao
             return g.copy(board = s) //returna o mesmo game so que com o board atualizado com a jogada feita no outro terminal de comandos
-            
         }
     }
+    "PLAY" to PlayCommand,
 )
 //temos um problema, pelo q estou a perceber vamos ter de fazer essa merda do mongo ou o crlh para guardar a informaçao, se virem agr o getCOmmands tem 1 parametro e o cassiano passa na mais uma merda do mongoDatabase, n sei se existe uma forma mais simples de fazer aquilo
 
