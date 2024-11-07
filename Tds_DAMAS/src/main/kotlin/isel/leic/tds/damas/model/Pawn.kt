9d9f8c882f9540f5262e7cffcd5d3869b1592660
Diogo Arnauth Fernandes Leitão <161584563+Diogoarnauth@users.noreply.github.com>
2024-11-07 14:25:31 +0000

    Update Pawn.kt

--- Tds_DAMAS/src/main/kotlin/isel/leic/tds/damas/model/Pawn.kt
+++ Tds_DAMAS/src/main/kotlin/isel/leic/tds/damas/model/Pawn.kt
@@ -2,19 +2,29 @@ package isel.leic.tds.damas.model
 
 class Pawn (player: Player) : Piece(player) {
     override fun canMove(to: Square, from: Square, board: Board): Boolean {
-        require(board.moves.keys.indexOf(to) == board.moves.keys.indexOf(from) - (BOARD_DIM - 1) ||
-                    board.moves.keys.indexOf(to) == board.moves.keys.indexOf(from) - (BOARD_DIM + 1) && Square(
-                board.moves.keys.indexOf(to)).black
-        ) { "Invalid Position" }
+
+        if(player== Player.w) {
+            require(
+                (board.moves.keys.indexOf(to) == board.moves.keys.indexOf(from) - (BOARD_DIM - 1) ) ||
+                        (board.moves.keys.indexOf(to) == board.moves.keys.indexOf(from) - (BOARD_DIM + 1) )
+            ) { "Invalid Position" }// não necessita verificar cantos pq é sempre branco
+            require(Square(board.moves.keys.indexOf(to)).black){"To isn't Black"}
+        }
+        else{
+            require(
+                board.moves.keys.indexOf(to) == board.moves.keys.indexOf(from) + (BOARD_DIM - 1) ||
+                        board.moves.keys.indexOf(to) == board.moves.keys.indexOf(from) + (BOARD_DIM + 1)
+            ) { "Invalid Position" }
+            require(Square(board.moves.keys.indexOf(to)).black){"To isn't Black"}
+        }
         require(board.moves.values.toList()[to.index] == null) { "Position already occupied" }
         return true
     }
 
-    override fun canKill(to: Square, from: Square, board: Board): Boolean {
+    override fun canKill(to: Square, from: Square, board: Board): Boolean {  //simplificar/otimizar
         if (board.player == Player.w) {
             require(
-                board.moves.keys.indexOf(to) == board.moves.keys.indexOf(from) - (BOARD_DIM * 2 - 2) ||
-                        board.moves.keys.indexOf(to) == board.moves.keys.indexOf(from) - (BOARD_DIM * 2 + 2)
+                board.moves.keys.indexOf(to) == board.moves.keys.indexOf(from) - (BOARD_DIM * 2 - 2) || board.moves.keys.indexOf(to) == board.moves.keys.indexOf(from) - (BOARD_DIM * 2 + 2)
             ) { "Invalid PLAY" }
             require(board.moves.values.toList()[(board.moves.keys.indexOf(from) - (BOARD_DIM - 1))] != null &&
                     board.moves.values.toList()[(board.moves.keys.indexOf(from) - (BOARD_DIM - 1))]?.player == board.player.other
