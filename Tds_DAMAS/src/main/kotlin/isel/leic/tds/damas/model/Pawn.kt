5b728f5c9366134037bdd65bcc3f0589b1b99279
Diogo Arnauth Fernandes Leitão <161584563+Diogoarnauth@users.noreply.github.com>
2024-11-09 17:38:34 +0000

    Update Pawn.kt

--- Tds_DAMAS/src/main/kotlin/isel/leic/tds/damas/model/Pawn.kt
+++ Tds_DAMAS/src/main/kotlin/isel/leic/tds/damas/model/Pawn.kt
@@ -46,5 +46,61 @@ class Pawn (player: Player) : Piece(player) {
         }
         return true
     }
+
+    override fun GenericCanKill(from: Square, board: Board): Boolean {
+        require(board.moves.values.toList()[board.moves.keys.indexOf(from)] != null){"Checking wrong square"}
+
+        if (player == Player.w && board.moves.keys.toList()[from.index].row.index > 1 ){
+            val NextLeft = board.moves.values.toList()[board.moves.keys.indexOf(from) - BOARD_DIM -1]
+            val NextRigth = board.moves.values.toList()[board.moves.keys.indexOf(from) - BOARD_DIM +1]
+
+            return (NextLeft ?.player == player.other
+                    && board.moves.values.toList()[board.moves.keys.indexOf(from) - BOARD_DIM * 2 - 2] == null
+                    && board.moves.keys.toList()[board.moves.keys.indexOf(from) - BOARD_DIM * 2 - 2].black
+                    ||
+                    NextRigth ?.player == player.other
+                    && board.moves.values.toList()[board.moves.keys.indexOf(from) - BOARD_DIM * 2 +2] == null
+                    && board.moves.keys.toList()[board.moves.keys.indexOf(from) - BOARD_DIM * 2 +2].black)
+
+        }else{
+            if( board.moves.keys.toList()[from.index].row.index < 6) {
+                val NextLeft = board.moves.values.toList()[board.moves.keys.indexOf(from) + BOARD_DIM - 1]
+                val NextRigth = board.moves.values.toList()[board.moves.keys.indexOf(from) + BOARD_DIM + 1]
+
+                return (NextLeft?.player == player.other
+                        && board.moves.values.toList()[board.moves.keys.indexOf(from) + BOARD_DIM * 2 - 2] == null
+                        && board.moves.keys.toList()[board.moves.keys.indexOf(from) + BOARD_DIM * 2 - 2].black
+                        ||
+                        NextRigth?.player == player.other
+                        && board.moves.values.toList()[board.moves.keys.indexOf(from) + BOARD_DIM * 2 + 2] == null
+                        && board.moves.keys.toList()[board.moves.keys.indexOf(from) + BOARD_DIM * 2 + 2].black)
+            }
+            return false
+        }
+    }
+
+    override fun GenericCanMove(from: Square, board: Board): Boolean { // testar ainda
+        if (player == Player.w) {
+            if (board.moves.keys.toList()[board.moves.keys.indexOf(from)].row.index != 0 // apenas para garantir
+                && (board.moves.values.toList()[board.moves.keys.indexOf(from) - BOARD_DIM - 1] == null ||
+                        board.moves.values.toList()[board.moves.keys.indexOf(from) - BOARD_DIM + 1] == null)
+            ) return true
+            else return false
+
+        } else {
+            if (board.moves.keys.toList()[board.moves.keys.indexOf(from)].row.index != BOARD_DIM - 1 // apenas para garantir
+                && (board.moves.values.toList()[board.moves.keys.indexOf(from) + BOARD_DIM - 1] == null ||
+                        board.moves.values.toList()[board.moves.keys.indexOf(from) + BOARD_DIM + 1] == null)
+            ) return true
+            else return false
+
+        }
+    }
+}
+fun getPawn(sq: Square) : Pawn?{
+    if(!sq.black ) return null
+    if(sq.row.index  in 0 .. 2) return Pawn(Player.b)
+    else if (sq.row.index  in BOARD_DIM - 3..BOARD_DIM-1 ) return Pawn(Player.w)
+    else return null
 }
 
