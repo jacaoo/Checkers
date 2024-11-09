4c23464ddccd7391daf92f57f76d5ee9c90f16c0
Diogo Arnauth Fernandes Leitão <161584563+Diogoarnauth@users.noreply.github.com>
2024-11-09 17:37:42 +0000

    Update Board.kt

--- Tds_DAMAS/src/main/kotlin/isel/leic/tds/damas/model/Board.kt
+++ Tds_DAMAS/src/main/kotlin/isel/leic/tds/damas/model/Board.kt
@@ -5,6 +5,7 @@ import kotlin.math.absoluteValue
 
 const val BOARD_DIM = 8
 const val BOARD_CELLS = BOARD_DIM * BOARD_DIM
+
 const val playerPieces = 12
 
 
@@ -14,6 +15,9 @@ enum class Player{
     val other get() = if(this == w) b else w
 
 }
+
+
+
 typealias Move = Pair<Square, Piece?> // É para associar um quadrado com uma peça ( w, b )
 
 typealias Moves = Map<Square, Piece?> // É uma espécie de lista para armazenar todos os possíveis espaços do tabuleiro
@@ -21,61 +25,55 @@ typealias Moves = Map<Square, Piece?> // É uma espécie de lista para armazenar
 fun Moves.renew (old: Square, new: Piece?) = this.plus(Move(old, new)) // Serve para adicionar a peça no board
 
 sealed class Board(val moves: Moves, val player: Player)
-class BoardRun(mvs: Moves, val turn: Player) : Board(mvs, turn)
+class BoardRun(mvs: Moves, val turn: Player, val FromSequenceMandatory: Square? = null) : Board(mvs, turn)
 class BoardWin(mvs: Moves, val winner: Player) : Board(mvs, winner)
 
 operator fun Board.get(position: Square): Player? = moves[position]?.player // Verifica qual é o player dentro do Quadrado Indicado
 
 fun initialBoard(): Board {
-    var init = emptyMap<Square,Piece?>()
-    Square.values.forEach { init = init.renew(it,null)}
-    var pieces = 0
-    var reverse = Square.values.size - 1
-    for (i in 0..<Square.values.size - 1) {
-        if ( Square(i).black ) {
-            init = init.renew(Square.values[i],Pawn(Player.b))
-            init = init.renew(Square.values[reverse],Pawn(Player.w))
-            reverse--
-            pieces++
-            if (pieces == playerPieces) break
-        }
-        else reverse--
-    }
+    val init = Square.values.map {  Pair(it, getPawn(it))}.toMap()
     return BoardRun(init,Player.w)
 }
 
+
+
 fun Board.play(from: Square, to: Square, player: Player): Board =
     when (this) {
-        is BoardWin -> error("Game is over")
-        else -> {
+        is BoardWin -> error("Game is over") // nunca vai funcionar pq precisa de uma nova iteração
+        is BoardRun -> {
+            if(this.FromSequenceMandatory != null) {
+                require(from == this.FromSequenceMandatory ){"there is a mandatory sequence"}
+            }
             val list = this.moves.values.toList()
+            require(this.moves.values.toList()[from.index] != null && this.moves.values.toList()[from.index]?.player == player){"You only can move your pieces"}
             require(this.moves.keys.toList()[to.index].black && this.moves.keys.toList()[from.index].black)
             {"Invalid Play"}
-            if (ThereIsAKill(player) || this.moves.values.toList()[from.index] is Queen ){
-                var newBoard = this
-                newBoard = newBoard.exchangekill(from, to, player)
-                if (newBoard.ThereIsAKill(player)) {
-                    newBoard = BoardRun(newBoard.moves, newBoard.player)
+            if (ThereIsAtLeastAKill(player)){
+
+                val actualPiece = this.moves.values.toList()[from.index]
+                val newBoard = this.exchangekill(from, to, player)
+
+                if (actualPiece!= null && actualPiece.GenericCanKill(to, newBoard)) { //corrigir o problema de quando tens varias kills possiveis e trocas de turn
+                     BoardRun(newBoard.moves, newBoard.player, to).CheckWin(player)
                 }
-                else newBoard = BoardRun(newBoard.moves, newBoard.player.other)
-                newBoard
+                else  BoardRun(newBoard.moves, newBoard.player.other).CheckWin(player)
+
 
             } else {
                 val piece =list[from.index]
                 require (piece != null && piece.canMove(to, from, this) ){"Invalid Position"}
                 val newb = this.exchange(from, to)
-                newb
+                val CheckedB= newb.CheckWin(player)
+                CheckedB
             }
             }
         }
 
-
-// Função exchangekill corrigida
 fun Board.exchangekill(from: Square, to: Square, player: Player): Board {
     val newMoves = this.moves.toMutableMap()
-    val piece = this.moves.values.toList()[from.index]
-    if (piece != null && piece.canKill(to, from, this)) {
-        if (piece is Pawn) {
+    val piece1 = this.moves.values.toList()[from.index]
+    if (piece1 != null && piece1.canKill(to, from, this)) {
+        if (piece1 is Pawn) {
 
             if (player == Player.w) {
                 if (this.moves.keys.indexOf(from) % BOARD_DIM > this.moves.keys.indexOf(to) % BOARD_DIM)
@@ -99,7 +97,7 @@ fun Board.exchangekill(from: Square, to: Square, player: Player): Board {
 
             val stepper = checkquarter(from,to)
             newMoves[Square(this.moves.keys.indexOf(to) - stepper)] = null
-            newMoves[to] = Queen(this.player)
+            newMoves[to] = newMoves[from]
             newMoves[from] = null
             return BoardRun(newMoves, player)
 
@@ -108,35 +106,13 @@ fun Board.exchangekill(from: Square, to: Square, player: Player): Board {
     }else return this
 }
 
-fun Board.ThereIsAKill(player: Player): Boolean {
+fun Board.ThereIsAtLeastAKill(player: Player): Boolean { // verifica no tabuleiro para um player se há pelo menos uma kill
     val list = this.moves.toMutableMap()
-    for ((sqr, pl) in list) {
-        if (pl != null && pl.player == player) {
-            if (player == Player.w) {
-                val middler = list.values.toList().getOrNull(sqr.index - (BOARD_DIM - 1))
-                val afterr = list.values.toList().getOrNull(sqr.index - (BOARD_DIM * 2 - 2))
-                val middlel = list.values.toList().getOrNull(sqr.index - (BOARD_DIM + 1))
-                val afterl = list.values.toList().getOrNull(sqr.index - (BOARD_DIM * 2 + 2))
-
-                if ((middlel != null && sqr.index% BOARD_DIM !in 0..1 && middlel.player == player.other && afterl == null) ||
-                    (middler != null && sqr.index% BOARD_DIM !in BOARD_DIM-2..BOARD_DIM-1 &&
-                            middler.player == player.other && afterr == null)) {
-                    return true
-                }
-            } else {
-                val middlel = list.values.toList().getOrNull(sqr.index + (BOARD_DIM - 1))
-                val afterl = list.values.toList().getOrNull(sqr.index + (BOARD_DIM * 2 - 2))
-                val middler = list.values.toList().getOrNull(sqr.index + (BOARD_DIM + 1))
-                val afterr = list.values.toList().getOrNull(sqr.index + (BOARD_DIM * 2 + 2))
-
-                if ((middlel != null && sqr.index% BOARD_DIM !in 0..1 && middlel.player == player.other && afterl == null) ||
-                    (middler != null && sqr.index% BOARD_DIM !in BOARD_DIM-2..BOARD_DIM-1
-                            && middler.player == player.other && afterr == null)) {
-                    return true
+    for ((sqr, piece) in list) {
+        if (piece != null && piece.player == player) {
+                if(piece.GenericCanKill(sqr, this)) return true
                 }
             }
-        }
-    }
     return false
 }
 
@@ -161,6 +137,37 @@ fun checkQueen(to: Square, player: Player) : Boolean {
         to.row.index == 0
     }
     else false
+}
+
+fun Board.CheckWin(player: Player): Board { //testar quando tivermos o serializer pq so mostra o win quando houver um board.win
+    if(this.NoMorePieces(player)) return BoardWin(this.moves, player.other)
+    else if(this.NoMorePieces(player.other)) return BoardWin(this.moves, player)
+    else{
+        if(this.IsLocked(player)) return BoardWin(this.moves, player.other)
+        else if(this.IsLocked(player.other)) return BoardWin(this.moves, player)
+    }
+    return this
+}
+
+fun Board.NoMorePieces(player: Player): Boolean {
+    val list = this.moves.toMutableMap()
+    for ((sqr, piece) in list) {
+        if (piece != null && piece.player == player) {
+           return false
+        }
+    }
+    return true
+}
+
+fun Board.IsLocked(player: Player): Boolean{
+    val list = this.moves.toMutableMap()
+    for ((sqr, piece) in list) {
+        if (piece != null && piece.player == player) {
+            if (piece.GenericCanKill(sqr, this) || piece.GenericCanMove(sqr, this)) return false
+        }
+    }
+    return true
+
 }
 
 
