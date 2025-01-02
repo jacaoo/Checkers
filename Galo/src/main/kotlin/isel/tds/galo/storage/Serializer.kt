bf43f628570ad58a4c45d0b6d10b12e3165f1198
jaco <bernardoesjaco@gmail.com>
2025-01-02 16:52:51 +0000

    Trabalho atual

--- Galo/src/main/kotlin/isel/tds/galo/storage/Serializer.kt
+++
@@ -1,6 +0,0 @@
-package isel.tds.galo.storage
-
-interface Serializer<Data> {
-    fun serialize(data: Data): String
-    fun deserialize(text: String): Data
-}
--- Tennis/src/main/kotlin/isel/tds/tennis/Player.kt
+++
@@ -1,5 +0,0 @@
-package isel.tds.isel.tds.tennis
-
-enum class Player {
-    A, B
-}
